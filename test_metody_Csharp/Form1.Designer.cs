03c03a91d53e3950434e948453e33b0d2fcc7927
Filip Iran <iranf.03@spst.cz>
2022-02-13 17:43:07 +0100

    v1.1

--- test_metody_Csharp/Form1.Designer.cs
+++ test_metody_Csharp/Form1.Designer.cs
@@ -28,13 +28,119 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.vstup = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.y = new System.Windows.Forms.TextBox();
+            this.x = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // vstup
+            // 
+            this.vstup.Location = new System.Drawing.Point(15, 26);
+            this.vstup.Name = "vstup";
+            this.vstup.Size = new System.Drawing.Size(121, 20);
+            this.vstup.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(142, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(78, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "První řešení";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(142, 41);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(78, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Druhé řešení";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(142, 70);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(78, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Třetí řešení";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // y
+            // 
+            this.y.Location = new System.Drawing.Point(118, 52);
+            this.y.Name = "y";
+            this.y.Size = new System.Drawing.Size(18, 20);
+            this.y.TabIndex = 4;
+            // 
+            // x
+            // 
+            this.x.Location = new System.Drawing.Point(72, 52);
+            this.x.Name = "x";
+            this.x.Size = new System.Drawing.Size(18, 20);
+            this.x.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Interval od";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(93, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(19, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "do";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(233, 102);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.x);
+            this.Controls.Add(this.y);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.vstup);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox vstup;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox y;
+        private System.Windows.Forms.TextBox x;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- test_metody_Csharp/Form1.cs
+++ test_metody_Csharp/Form1.cs
@@ -16,5 +16,78 @@ namespace test_metody_Csharp
         {
             InitializeComponent();
         }
+
+        public void Faktorial(short vstup2)
+        {
+            int cislo = 1;
+
+            for (int i = 1; i <= vstup2; i++)
+            {
+                cislo = cislo* i;
+            }
+
+            short vysledek = short.Parse(Convert.ToString(cislo));
+
+            MessageBox.Show("Faktoriál čísla " + vstup2 + " je " + vysledek, "Výsledek");
+        }
+
+        public long Faktorial(long vstup2)
+        {
+            if (vstup2 == 0)
+            {
+                return 1;
+            }
+            return vstup2 * Faktorial((vstup2 - (long)1));
+        }
+
+        public void Faktorial(int x, int y)
+        {
+            long vysledek = 0;
+
+            for (int i = x; i <= y; i++)
+            {
+                vysledek = Faktorial((long)i);
+
+                MessageBox.Show("Faktoriál čísla " + i + " je " + vysledek, "Výsledek");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Faktorial((short)Convert.ToInt32(vstup.Text));
+            }
+            catch
+            {
+                MessageBox.Show("Musíš zadat hodnoty!", "Error");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Faktorial((long)Convert.ToInt32(vstup.Text));
+
+                MessageBox.Show("Faktoriál čísla " + (long)Convert.ToInt32(vstup.Text) + " je " + Faktorial((long)Convert.ToInt32(vstup.Text)), "Výsledek");
+            }
+            catch
+            {
+                MessageBox.Show("Musíš zadat hodnoty!", "Error");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Faktorial(Convert.ToInt32(x.Text), Convert.ToInt32(y.Text));
+            }
+            catch
+            {
+                MessageBox.Show("Musíš zadat hodnoty!", "Error");
+            }
+        }
     }
 }
